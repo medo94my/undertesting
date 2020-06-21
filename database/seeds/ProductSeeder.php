5949183290e362b4f52a8f5318195c25c43b174b
ahmed tawfik <medoroyalrma@gmail.com>
2020-06-21 19:51:33 +0800

    updated files

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -21,7 +21,7 @@ class ProductSeeder extends Seeder
             DB::table('products')->insert([
                 'name' => 'Product-'.$i,
                 'image' => 'img/product/product-'.$img_counter.'.jpg',
-                'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
                 'discount'=> $i%2==0 ? 0: ($i%3==0 ? 0: ($i%3)*rand(10,20)),
                 'price' => rand(149999, 249999),
                 'weight' => rand(10,1000),
